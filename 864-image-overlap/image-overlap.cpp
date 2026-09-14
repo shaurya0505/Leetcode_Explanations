1b3770888676619ff89b8788246d3ed584627a13
Shaurya Agarwal <147256995+shaurya0505@users.noreply.github.com>
2026-09-14 13:35:39 +0530

    Time: 43 ms (77.41%) | Memory: 13 MB (68.90%) - LeetSync

--- 864-image-overlap/image-overlap.cpp
+++ 864-image-overlap/image-overlap.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
+    int overlap(vector<vector<int>>& img1, vector<vector<int>>& img2, int row_offset, int col_offset){
+        int n=img1.size();
+        int cnt=0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i+row_offset>=0 and i+row_offset<n and j+col_offset>=0 and j+col_offset<n){
+                    if(img1[i+row_offset][j+col_offset] and img2[i][j]) cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
     int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
-        int n = img1.size();
-        vector<pair<int,int>> A, B;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (img1[i][j] == 1) A.emplace_back(i, j);
-                if (img2[i][j] == 1) B.emplace_back(i, j);
+        int n=img1.size();
+        int bst=0;
+        for(int rof=-n+1; rof<n; rof++){
+            for(int cof=-n+1; cof<n; cof++){
+                int orlp= overlap(img1,img2,rof,cof);
+                bst= max(bst,orlp);
             }
         }
-        vector<vector<int>> cnt(2 * n, vector<int>(2 * n, 0));
-        int best = 0;
-        for (auto& a : A) {
-            for (auto& b : B) {
-                int dx = b.first - a.first + n;
-                int dy = b.second - a.second + n;
-                best = max(best, ++cnt[dx][dy]);
-            }
-        }
-        return best;
+        return bst;
     }
 };
