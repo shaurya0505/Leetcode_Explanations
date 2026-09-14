bc81c18a9118be18a06fd3f75b297a1185a1bd26
Shaurya Agarwal <147256995+shaurya0505@users.noreply.github.com>
2026-09-14 15:07:06 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (52.28%) - LeetSync

--- 174-dungeon-game/dungeon-game.cpp
+++ 174-dungeon-game/dungeon-game.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int m,n;
-    int t[201][201];
-    int solve(int i, int j, vector<vector<int>>& d){
-        if(i>=m || j>=n) return 1e9;
-        if(t[i][j]!=-1) return t[i][j];
-        if(i==m-1 and j==n-1){
-            if(d[i][j]>0) return 1;
-            return abs(d[i][j])+1;
-        }
-        int rgt= solve(i,j+1,d);
-        int dwn= solve(i+1,j,d);
-
-        int res= min(rgt,dwn)-d[i][j];
-
-        return t[i][j]= res>0 ? res : 1;
-    }
     int calculateMinimumHP(vector<vector<int>>& d) {
-        m=d.size();
-        n=d[0].size();
-        memset(t,-1,sizeof(t));
-        return solve(0,0,d);
+        int m=d.size();
+        int n=d[0].size();
+        vector<vector<int>>t(m,vector<int>(n,0));
+
+        for(int i=m-1; i>=0; i--){
+            for(int j=n-1; j>=0; j--){
+                if(i==m-1 and j==n-1){
+                    t[i][j]= d[i][j]>0 ? 1 : abs(d[i][j])+1;
+                }
+                else{
+                    int dwn= (i<m-1) ? t[i+1][j] : 1e9;
+                    int rgt= (j<n-1) ? t[i][j+1] : 1e9;
+
+                    int res= min(dwn,rgt)- d[i][j];
+                    t[i][j]= (res>0) ? res : 1;
+
+                }
+            }
+        }
+        return t[0][0];
     }
 };
